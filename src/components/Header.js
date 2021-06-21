3c21fe4ac98e9b682e7d1465de182682e33bafcf
RaphaelHardFork <raphael.pellet@protonmail.com>
2021-06-21 17:24:55 +0200

    creation

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import { Box, Text, Heading } from "@chakra-ui/react"
+
+const Header = () => {
+  return (
+    <>
+      <Box bg="blackAlpha.900">
+        <Heading py="10" color="white">
+          Raphael Pellet
+        </Heading>
+      </Box>
+    </>
+  )
+}
+
+export default Header
---
+++ src/components/LandingPage.js
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react"
+import Header from "./Header"
+
+const LandingPage = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default LandingPage
